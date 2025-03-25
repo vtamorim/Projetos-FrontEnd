8419c9f8494f1b7b019f64f4123e4212a93a2946
Victor Miguel <vthero89@gmail.com>
2025-03-25 10:07:23 -0300

    Calculadora Feita

--- Calculadora/calcu.js
+++ Calculadora/calcu.js
@@ -1,7 +1,8 @@
 
 num_display = document.getElementById("num_display")
 
-
+span_sec = document.getElementById("span_sec")
+condition = ""
 document.querySelectorAll(".btn").forEach(button => {
     button.addEventListener("click", () => {
         num_display.textContent += button.value
@@ -10,7 +11,65 @@ document.querySelectorAll(".btn").forEach(button => {
 
 
   });
+clear_cond = 0
 document.getElementById("clear").addEventListener("click", () => {
-    num_display.textContent  =""
+    clear_cond +=1 
+    if(clear_cond == 1){
+        num_display.textContent = ""
+    }
+    else if(clear_cond == 2){
+        span_sec.textContent = ""
+    }
+
 })
 
+document.getElementById("mais").addEventListener("click", () => {
+   condition = "mais"
+})
+
+document.getElementById("menus").addEventListener("click", () => {
+    condition = "menos"
+})
+
+document.getElementById("divi").addEventListener("click", () => {
+    condition = "divisão"
+})
+
+document.getElementById("multi").addEventListener("click", () => {
+    condition = "multiplicação"
+})
+
+
+
+document.querySelectorAll(".operator").forEach(button => {
+    button.addEventListener("click", () => {
+        if (!num_display.textContent == ""){
+            span_sec.textContent  = num_display.textContent
+            num_display.textContent  = ""
+        }
+    })
+});
+
+
+
+
+document.getElementById("equals").addEventListener("click", () => {
+    switch(condition){
+        case "mais":
+            num_display.textContent = Number(num_display.textContent) + Number(span_sec.textContent)
+            break
+        case "menos":
+            num_display.textContent = Number(num_display.textContent) - Number(span_sec.textContent)
+            break
+        case "multiplicação":
+            num_display.textContent = Number(num_display.textContent) * Number(span_sec.textContent)
+            break
+        case "divisão":
+            num_display.textContent = Number(span_sec.textContent) / Number(num_display.textContent)
+            break
+    
+    default:
+    
+        }
+    span_sec.textContent = ""
+})
