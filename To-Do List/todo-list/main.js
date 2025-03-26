942ea854a0c381ea87d7dc5defd7bbaf847048c9
Victor Miguel <vthero89@gmail.com>
2025-03-26 10:48:47 -0300

    Criando uma To-Do-List

---
+++ To-Do List/todo-list/main.js
@@ -0,0 +1,8 @@
+add_list = document.getElementById("add_list");
+button = document.getElementById("button");
+
+list_do = document.getElementById("list_do");
+
+async function executar(resultado) {
+  resultado
+}
