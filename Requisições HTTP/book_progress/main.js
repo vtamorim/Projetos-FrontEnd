8f894222902d27af06b501055c7198f2a6a0760b
Victor Miguel <vthero89@gmail.com>
2025-04-14 13:56:42 -0300

    Vo me enlouquecer

---
+++ Requisições HTTP/book_progress/main.js
@@ -0,0 +1,6 @@
+const namebook = document.getElementById("namebook");
+const button = document.getElementById("button");
+
+button.addEventListener("click", () => {
+    console.log(namebook.textContent)
+});
