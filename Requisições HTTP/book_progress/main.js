3fe8d6c500a3f0359a3c0f9248e5bb0b6b219ed4
Victor Miguel <vthero89@gmail.com>
2025-04-29 16:58:51 -0300

    Fazendo Script Parcial

--- Requisições HTTP/book_progress/main.js
+++ Requisições HTTP/book_progress/main.js
@@ -1,6 +1,24 @@
-const namebook = document.getElementById("namebook");
-const button = document.getElementById("button");
 
-button.addEventListener("click", () => {
-    console.log(namebook.textContent)
+book_form = document.getElementById("book_form");
+book_form.addEventListener("submit", (event) => {
+
+    event.preventDefault();
+    const namebook = document.getElementById("namebook").value;
+    const pages_total = parseInt(document.getElementById("pages_total").value);
+    const pages_read = parseInt(document.getElementById("pages_read").value);
+
+    localStorage.setItem(namebook, JSON.stringify({name: namebook, total : pages_total, read: pages_read}));
+
+
+    
+   const progress_save = JSON.parse(localStorage.getItem(namebook)) 
+
+   livro_info = document.getElementById("livro_info");
+
+   if(progress_save){
+    livro_info.innerHTML = `
+        <p>${progress_save.name}</p>
+        <progress value = "${progress_save.read}" max = "${progress_save.total}"></progress>
+    `;
+   }
 });
